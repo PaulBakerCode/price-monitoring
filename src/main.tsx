3dd5e7df81d3c6664d0a106f0b8204176297c552
Paul Baker <paul.baker.code@gmail.com>
2025-01-18 16:09:43 +0300

    refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
-const isProdaction = process.env.NODE_ENV === 'production';
-console.log(isProdaction);
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/price-monitoring/' : '/'}>
